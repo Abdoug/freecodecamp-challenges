705dfd73d36c05696695753992dd16d55c0deab4
Ougouag <abderrahmane.ougouag@cgi.com>
2022-07-11 16:41:59 -0400

    adding implement merge sort challenge

---
+++ Challenges/Algorithms/ImplementMergeSort.js
@@ -0,0 +1,27 @@
+function mergeSort(array) {
+    const arrayLength = array.length
+    const pivotIndex = Math.floor(arrayLength / 2)
+
+    if (arrayLength <= 1) return array
+
+    const leftArray = array.splice(0, pivotIndex)
+    const rightArray = array
+
+    return merge(mergeSort(leftArray), mergeSort(rightArray));
+}
+  
+function merge(firstArray, secondArray) {
+    const sortedArray = []
+
+    while (firstArray.length && secondArray.length) {
+        if (firstArray[0] <= secondArray[0]) {
+        sortedArray.push(firstArray.shift())
+        } else {
+        sortedArray.push(secondArray.shift())
+        }
+    }
+
+    return [...sortedArray, ...firstArray, ...secondArray]
+}
+  
+mergeSort([10, 2, 1, 6, 3, 9, 7])
